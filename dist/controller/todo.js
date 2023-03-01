9f569ff5a503f80e3602da0a7e6dd2f3734444b1
Otulugbu Alexander <s2200742@Alexander-MacBook-Air.local>
2023-03-01 18:11:07 +0200

    My first commit

--- dist/controller/todo.js
+++ dist/controller/todo.js
@@ -1,39 +1,59 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteTodo = exports.updateTodo = exports.getTodo = exports.createTodo = void 0;
-const todo_1 = require("../models/todo");
-const TODOS = [];
-const createTodo = (req, res, next) => {
-    const text = req.body.text;
-    const newTodo = new todo_1.Todo(Math.random().toString(), text);
-    TODOS.push(newTodo);
-    res.status(201).json({ message: 'Create the todo', createTodo: newTodo });
+const todo_1 = __importDefault(require("../models/todo"));
+//const TODOS: Todo[] = [];
+const createTodo = async (req, res, next) => {
+    try {
+        const data = req.body;
+        console.log('Data', data);
+        let todos = await todo_1.default.create(data);
+        return res
+            .status(200)
+            .json({ message: 'Todo created sucessfully', data: todos });
+    }
+    catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
 };
 exports.createTodo = createTodo;
-const getTodo = (req, res, next) => {
-    res.json({ todos: TODOS });
+const getTodo = async (req, res, next) => {
+    try {
+        let todos = await todo_1.default.find({});
+        return res.status(200).json({ message: 'All todos', data: todos });
+    }
+    catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
 };
 exports.getTodo = getTodo;
-const updateTodo = (req, res, next) => {
-    const todoId = req.params.id;
-    const updatedText = req.body.text;
-    const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
-    if (todoIndex < 0) {
-        throw new Error('I could no t fine todo');
+const updateTodo = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        let todos = await todo_1.default.findByIdAndUpdate(id, req.body, { new: true });
+        return res
+            .status(200)
+            .json({ message: 'Todo updated succesfully', data: todos });
     }
-    TODOS[todoIndex] = new todo_1.Todo(TODOS[todoIndex].id, updatedText);
-    res.json({ message: "Updated", updateTodo: TODOS[todoIndex] });
+    catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
 };
 exports.updateTodo = updateTodo;
+// add deleteTodo method
 const deleteTodo = (req, res, next) => {
-    const todoId = req.params.id;
-    const todoIndex = TODOS.findIndex((todo) => todoId === todo.id);
-    if (todoIndex < 0) {
-        throw new Error('todo not found');
+    try {
+        const { id } = req.params;
+        let todos = todo_1.default.findByIdAndDelete(id);
+        return res
+            .status(200)
+            .json({ message: 'Todo deleted succesfully', data: todos });
     }
-    else {
-        TODOS.splice(todoIndex, 1);
-        res.status(200).json({ message: 'Item deleted' });
+    catch (error) {
+        return res.status(500).json({ message: error.message });
     }
 };
 exports.deleteTodo = deleteTodo;
--- src/controller/todo.ts
+++ src/controller/todo.ts
@@ -1,43 +1,52 @@
-import { RequestHandler } from "express";
-import { Todo } from '../models/todo';
+import { RequestHandler } from 'express';
+import Todo, { TodoModel } from '../models/todo';
 
-const TODOS: Todo[] = [];
+//const TODOS: Todo[] = [];
 
-export const createTodo: RequestHandler =  ( req, res, next) => {
-
-    const text = (req.body as {text: string}).text;
-
-    const newTodo = new Todo(Math.random().toString(), text);
-    TODOS.push(newTodo);
-    res.status(201).json({message: 'Create the todo', createTodo: newTodo});
-}; 
-
-export const getTodo: RequestHandler =  ( req, res, next) => {
-  res.json({todos: TODOS});
-}; 
-
-export const updateTodo: RequestHandler< {id: string} >=  ( req, res, next) => {
-
-  const todoId = req.params.id;
-
-  const updatedText = (req.body as {text: string}).text;
-  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
-
-  if (todoIndex < 0 ){
-    throw new Error('I could no t fine todo');
+export const createTodo: RequestHandler = async (req, res, next) => {
+  try {
+    const data: TodoModel = req.body;
+    console.log('Data', data);
+    let todos = await Todo.create(data);
+    return res
+      .status(200)
+      .json({ message: 'Todo created sucessfully', data: todos });
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
   }
-  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
-  res.json({message: "Updated", updateTodo: TODOS[todoIndex]});
-}; 
+};
+
+export const getTodo: RequestHandler = async (req, res, next) => {
+  try {
+    let todos = await Todo.find({});
+    return res.status(200).json({ message: 'All todos', data: todos });
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateTodo: RequestHandler = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    let todos = await Todo.findByIdAndUpdate(id, req.body, { new: true });
+    return res
+      .status(200)
+      .json({ message: 'Todo updated succesfully', data: todos });
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// add deleteTodo method
 
 export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
-  const todoId = req.params.id
-
-  const todoIndex = TODOS.findIndex((todo) => todoId === todo.id);
-  
-  if (todoIndex < 0) {
-    throw new Error('todo not found');
-  } else { 
-    TODOS.splice(todoIndex, 1);
-    res.status(200).json({ message: 'Item deleted' })
-  }}
+  try {
+    const { id } = req.params;
+    let todos = Todo.findByIdAndDelete(id);
+    return res
+      .status(200)
+      .json({ message: 'Todo deleted succesfully', data: todos });
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+}
